264fbb009fbd8cdb65f5794929ee2dcd6f36de6b
hevieri <hevieri@duck.com>
2026-05-01 21:17:38 -0300

    ingreso usuario y contraseña

--- test_login.py
+++ test_login.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 try:
+    #Ingreso pagina
     driver.get("https://www.saucedemo.com/")
 
-#Ingresar usuario
+    #Ingresar usuario
+    usuario = driver.find_element(By.ID, "user-name")
 
 finally:
     driver.quit()
