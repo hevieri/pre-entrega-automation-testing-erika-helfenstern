82b8c7694000c65043773c6255366ed2c02b7f66
hevieri <hevieri@duck.com>
2026-05-03 20:27:48 -0300

    agrego tecla enter y validador de pagina

--- test_login.py
+++ test_login.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Chrome()
 try:
     #Ingreso pagina
@@ -7,6 +9,20 @@ try:
 
     #Ingresar usuario
     usuario = driver.find_element(By.ID, "user-name")
+    usuario.send_keys("standard_user")
+
+    #Ingresar contraseña
+    password = driver.find_element(By.ID, "password")
+    password.send_keys("secret_sauce")
+
+    #Hacer click en el botón de login
+    password.send_keys(keys.RETURN)
+
+    #Verificar url
+    if "/inventory.html" in driver.current_url:
+        print("Pagina correcta")
+    else:
+        print("No es la página de inventario")
 
 finally:
     driver.quit()
